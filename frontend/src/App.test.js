f54ac12d3cd149726ffe759b3b5ffa4356d95a10
zach <zaclewis@gmail.com>
2024-11-22 08:04:42 -0600

    fix test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,30 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/User List/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Component', () => {
+  // Test for rendering the header
+  test('should render the User List header', () => {
+    render(<App />);
+    const headerElement = screen.getByText(/User List/i);
+    expect(headerElement).toBeInTheDocument();
+  });
+
+  // Test for header correct HTML element
+  test('should render User List in an h1 tag', () => {
+    render(<App />);
+    const headerElement = screen.getByRole('heading', { level: 4, name: /User List/i });
+    expect(headerElement).toBeInTheDocument();
+  });
+
+  // Test for accessibility
+  test('should meet basic accessibility standards', () => {
+    render(<App />);
+    const heading = screen.getByRole('heading', { level: 4 });
+    expect(heading).toBeVisible();
+    expect(heading).toHaveAccessibleName();
+  });
+
+
+
 });
